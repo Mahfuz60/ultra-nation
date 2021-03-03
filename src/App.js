7283d5a55fed1208868b946fd5cedec2e3926844
Mahfuz <you@example.com>
2021-03-04 02:52:25 +0600

    nation code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const[countries,setCountries]=useState([]);
+  useEffect(() => {
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then((response) =>response.json())
+    .then(data =>setCountries(data))
+   
+  },[]);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>Country Loaded:{countries.length}</h1> 
+       <ol>
+       {
+         countries.map((country) =><li>{country.name}</li>)
+       }
+       </ol>
+        
       </header>
     </div>
   );
